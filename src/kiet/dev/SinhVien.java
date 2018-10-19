6c0f3b2ae308dfef8fbb93153dfd5aa9b7ec36dc
Kiet <206love193@gmail.com>
2018-10-19 22:43:24 +0700

    Finish sinhvien diemtrungbinh

--- src/kiet/dev/SinhVien.java
+++ src/kiet/dev/SinhVien.java
@@ -1,8 +1,23 @@
 package kiet.dev;
 
 public class SinhVien {
+	  
+	private String name;
+	private double Toan;
+	private double Ly;
+	private double Hoa;
 	
 	public SinhVien(String name, double Toan, double Ly, double Hoa) {
-
+			
+		this.name = name;
+		this.Toan = Toan;
+		this.Ly = Ly;
+		this.Hoa = Hoa;
+			}
+    public double DiemTB(double Toan, double Ly, double Hoa) {
+   	 return (Toan + Ly + Hoa) / 3;
+		}
+    public void DiemTB() {
+	  System.out.println(name+":"+DiemTB(Toan, Ly, Hoa));
 	}
 }
